3f999d74a92f7b265b2f393a2efc684336eddfea
Senmiao Liu <zir.echo@gmail.com>
2013-02-14 11:54:23 +0800

    Add MIT license info

--- engine.io_parser.h
+++ engine.io_parser.h
@@ -1,13 +1,32 @@
-//
-//  engine.io_parser.h
-//  C implementation of Engine.IO protocol
-//
-//  Created by Liu Senmiao on 2/10/13.
-//  Copyright (c) 2013 Liu Senmiao. All rights reserved.
-//
-
 #ifndef engine_io_parser_h
 #define engine_io_parser_h
+/*
+ * engine.io_parser.h
+ * C implementation of Engine.IO protocol
+ *
+ * Copyright (c) 2013 Liu Senmiao. All rights reserved.
+ *
+ * (The MIT License)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * 'Software'), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 #ifdef __cplusplus
 extern "C" {
 #endif
